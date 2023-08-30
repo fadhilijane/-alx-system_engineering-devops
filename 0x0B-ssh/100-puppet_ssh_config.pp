9dd1999cd3f6611ab66b031597d0342e17c01814
fadhilijane <janefadhili@gmail.com>
2023-08-30 06:21:01 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,18 @@
 # set up your client SSH configuration file so
 #that you can connect to a server without typing a password
 
-file_line {'remove password authentication':
-  ensure  => 'present',
+include stdlib
+
+file_line {'remove PasswordAuthentication':
+  ensure  => present,
   path    => 'etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  match   => 'PasswordAuthentication yes',
   replace => true,
 }
 
 file_line {'add the private key':
-  ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
+  ensure  => present,
+  path    => 'etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  replace => true'
 }
