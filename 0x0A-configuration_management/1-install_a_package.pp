2bbdb57c0bef39c692c0ade628f1defd74654f8d
fadhilijane <janefadhili@gmail.com>
2024-05-29 18:40:10 +0000

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,4 @@
-#!/usr.bin/pip
+#!/usr/bin/pip
 #Using Puppet, install flask from pip3
 package {'python3-pip':
   ensure => installed,
@@ -6,6 +6,5 @@ package {'python3-pip':
 
 exec {'install_flask':
   command => '/usr/bin/pip3 install flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',  
   require => Package['python3-pip'],
 }
