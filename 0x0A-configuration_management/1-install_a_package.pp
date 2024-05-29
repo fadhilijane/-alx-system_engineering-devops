684542e2457b32b4f1c72ba9b2bf2f8736fb64e3
fadhilijane <janefadhili@gmail.com>
2024-05-29 18:54:21 +0000

    config

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -6,6 +6,6 @@ package {'python3-pip':
 
 exec {'install_flask':
   ensure   => '2.1.0',
-  command  => '/usr/bin/pip3 install flask==2.1.0',
+  command  => '/usr/local/lib/python3.8 install flask==2.1.0',
   provider => Package['python3-pip'],
 }
