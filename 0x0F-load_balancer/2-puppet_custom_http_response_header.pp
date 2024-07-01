2510bd7ef5cf4fa6659405eb67516cdba3b17db6
fadhilijane <janefadhili@gmail.com>
2024-07-01 09:48:08 +0000

    k

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+# 2-puppet_custom_http_response_header.pp
+
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}
